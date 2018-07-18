dc366393ebdb5c5cee57917e624fcdcba3c29bfa
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-07-18 16:28:11 +0700

    rename and add pull method

---
+++ src/HtmlTransport.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LiveCMS\Transport;
+
+class HtmlTransport
+{
+    const DEFAULT = 'default';
+
+    protected static $html = [];
+
+    public function set($html = null, $name = null)
+    {
+        $name = $name ?? static::DEFAULT;
+        static::$html[$name] = $html;
+    }
+
+    public function append($html, $name = null)
+    {
+        $name = $name ?? static::DEFAULT;
+        if (!isset(static::$html[$name])) {
+            static::$html[$name] = '';
+        }
+        static::$html[$name] .= "\r\n".$html;
+        return $this;
+    }
+
+    public function get($name = null)
+    {
+        $name = $name ?? static::DEFAULT;
+        return static::$html[$name] ?? null;
+    }
+
+    public function pull($name = null)
+    {
+        $html = $this->get($name);
+        $this->set(null, $name);
+        return $html;
+    }
+
+    public function flush()
+    {
+        $this->set();
+    }
+}
